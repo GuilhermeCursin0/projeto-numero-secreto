dcf76d41397700766a2e26077833e3ec0fac7239
Guilherme Cursino <guilherme.f96@outlook.com>
2024-02-28 12:15:17 -0300

    Organização do desafio

--- desafio2.js
+++ desafio2.js
@@ -13,7 +13,7 @@
     Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
 */
 
-// imc = peso/ altura^2
+// 1  imc = peso/ altura^2
 function calculaImc(peso, altura){
     if (altura > 3){
         return peso/(altura*altura)*10**4;
@@ -22,7 +22,7 @@ function calculaImc(peso, altura){
         return peso/(altura*altura); 
     }
 }
-
+// 2 - Fatorial de um numero dado
 function fatorial(num){
     let resultado= 1;
     let contador= 1;
@@ -33,7 +33,7 @@ function fatorial(num){
     return resultado;
 }
 
-/// Outra Forma de fazer fatorial
+// Outra Forma de fazer fatorial
     function fato(xx) { 
         let fat = xx; 
         if (fat == 1){ 
@@ -44,21 +44,25 @@ function fatorial(num){
     }
 //
 
+// 3 - Converte valor em dolar para um valor em real
 function cambioDolarReal(dolar){
     return dolar*4.80;
 }
 
+// 4 - Calcula area e perimetro
 function areaPerimetro(altura, largura){
     let area = altura*largura;
     let perimetro = (altura+largura)*2;
     alert('Area = '+ area +"  Perimetro = " + perimetro);
 }
 
+// 5 - Calcula Area circulo
 function areaCirculo(raio){
     let area = 3.14*raio**2;
     console.log(area);
 }
 
+// 6 - Imprime no console a tabuada de um numero dado
 function tabuada(valor){
     let cont=1;
     while(cont<=10){
